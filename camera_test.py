8ec77ca6dd47429e67670d74b0b3c9dcb9f49cd0
ImDyslectik <robin.alkema@student.nhlstenden.com>
2022-05-09 13:22:20 +0200

    it goes fast

--- camera_test.py
+++ camera_test.py
@@ -2,31 +2,38 @@ import cv2
 import numpy as np
 import math
 
-vid = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 while(True):
-    ret,frame = vid.read()
+    ret,frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #lower_blue = np.array([60, 35, 140])
+    #upper_blue = np.array([180, 255, 255])
+
+    lower_light_blue = np.array([90,50,50])
+    upper_light_blue = np.array([130,255,255])
+    mask_blue = cv2.inRange(hsv, lower_light_blue, upper_light_blue)
+    detected_output_blue = cv2.bitwise_and(frame, frame, mask =  mask_blue)
     
-    lower_blue = np.array([130,0,0])
-    upper_blue = np.array([255,180,120])
-    maskblue = cv2.inRange(frame, lower_blue, upper_blue)
-    blue = cv2.bitwise_and(frame, frame, mask =  maskblue)
-    
-    ret,th = cv2.threshold(maskblue,180,255,cv2.THRESH_BINARY_INV)
+    ret,th = cv2.threshold(mask_blue,180,255,cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
-    for cont in range(len(contours)):
-        contour = contours[cont]
-        area = cv2.contourArea(contour)
-        perimeter = cv2.arcLength(contour, True)
-        if area > 0 and perimeter > 0:
-            factor = 4 * math.pi * area / perimeter**2
+    hierarchy = hierarchy[0]
+    for cnt in range(len(contours)):
+        area = cv2.contourArea(contours[cnt])
+        if area < 500:
+            continue
+        M = cv2.moments(contours[cnt])
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
 
-        if   factor > 0.25 and factor < 50 and area > 150:
-            cv2.drawContours(frame, [contour], -1, (0,255,255), 3)
+        cv2.circle(frame, (cx, cy), 2, (0,255,255), 2)
     
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('b'):
+    cv2.imshow("Blue", detected_output_blue)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-vid.release()
+
+cv2.waitKey(0) 
 cv2.destroyAllWindows()
