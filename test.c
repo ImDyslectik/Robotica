26d48469815b320347d06fd1665317ecc16b4337
ImDyslectik <robin.alkema@student.nhlstenden.com>
2022-05-17 12:28:35 +0200

    working with engine in c
    samueeel

--- test.c
+++ test.c
@@ -1,20 +1,32 @@
 #include <wiringPi.h>
+#include <softPwm.h>
 
 int main(void)
 {
-    // Red LED: Physical pin 18, BCM GPIO24, and WiringPi pin 5.
-    const int led = 5;
+    const int pinA = 23;
+    const int pinB = 26;
+    const int delay = 5000;
+
+    softPwmCreate(pinA,0,5);
+    softPwmCreate(pinB,0,5);
 
     wiringPiSetup();
 
     pinMode(led, OUTPUT);
 
     while (1) {
-        digitalWrite(led, HIGH);
-        delay(500);
-        digitalWrite(led, LOW);
-        delay(500);
+        softPwmWrite(pinA,0);
+        softPwmWrite(pinB,0);
+        delay(delay);
+        softPwmWrite(pinA,2);
+        softPwmWrite(pinB,0);
+        delay(delay);
+        softPwmWrite(pinA,5);
+        softPwmWrite(pinB,0);
+        delay(delay);
+        softPwmWrite(pinA,2);
+        softPwmWrite(pinB,0);
+        delay(delay);
     }
-
     return 0;
 }
